460cf21db6c040e2830fc0194d6f904649a1dea7
Sandro Pischinger <mail@sandropischinger.de>
2025-06-11 09:41:36 +0200

    fix digest hex string

--- kong/plugins/nx-wh-hmac/handler.lua
+++ kong/plugins/nx-wh-hmac/handler.lua
@@ -1,21 +1,22 @@
 local openssl_mac = require "resty.openssl.mac"
+local str = require "resty.string"
 
 local HEADERS_NX_SIG = "X-Nexudus-Hook-Signature"
 
-local NxWhHmacHandler = {
-  VERSION = "0.0.0",
+local plugin = {
   PRIORITY = 1029,
+  VERSION = "0.0.0",
 }
 
-function NxWhHmacHandler:access(conf)
+function plugin:access(plugin_conf)
   local headers = kong.request.get_headers()
   local sig_received = headers[HEADERS_NX_SIG]
   if not sig_received then
-    return nil, { status=401, message="no nexudus signature header given" } }
+    return kong.response.error(401, "nexudus header sig missing")
   end
 
   if type(sig_received) ~= "string" then
-    return nil, { status=400, message="header sig is not string" } }
+    return kong.response.error(400, "nexudus header sig invalid format")
   end
 
   local body, err = kong.request.get_raw_body()
@@ -23,13 +24,13 @@ function NxWhHmacHandler:access(conf)
     return {}
   end
 
-  local sig, err = openssl_mac.new(conf.shared_secret, "hmac", nil, "sha256"):final(body or '')
-  local sig_calculated = string.upper(sig)
+  local digest, err = openssl_mac.new(plugin_conf.shared_secret, "hmac", nil, "sha256"):final(body or '')
+  local sig_calculated = string.upper(str.to_hex(digest))
 
   if sig_received ~= sig_calculated then
-    return nil, { status=401, message="nexudus signature mismatch" } }
+    return kong.response.error(401, "nexudus signature mismatch")
   end
 
 end
 
-return NxWHmacHandler
+return plugin
